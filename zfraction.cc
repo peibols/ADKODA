7553f2172103251b6458e6a70cd4ef9868d4f380
Daniel Pablos <peibols@pc-104-18.eduroam.uib.no>
2019-03-19 09:24:27 +0100

    adding z sampling

---
+++ zfraction.cc
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_roots.h>
+#include <gsl/gsl_errno.h>
+#include <iostream>
+
+using namespace std;
+
+double int0_P_gg(double t0, double t2)
+{
+  double T=t0/t2;
+  double CA=3.;
+  return 
+}
